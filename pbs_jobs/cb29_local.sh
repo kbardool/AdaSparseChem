1a0a20fd8fd7d36bc36b74460b64a7c3d985797b
Kevin Bardool <kbardool@aol.com>
2022-08-03 17:22:35 +0200

    03-08-2022 Cleanup cb29_xx_local scripts

--- pbs_jobs/cb29_local.sh
+++ pbs_jobs/cb29_local.sh
@@ -8,23 +8,14 @@ arch_type=$1
 submit_type="LOCAL"
 # PBS Parameters =========================================================
 # PBS -M kevin.bardool@kuleuven.be
-# PBS -l pmem=5gb,
-# PBS -l qos=debugging
-# pbs_account="-A lp_symbiosys "
-# pbs_folders="-e ../pbs_output/  -o ../pbs_output/ "
-# pbs_allocate="-l nodes=1:ppn=9:gpus=1,partition=gpu,walltime=72:00:00 "
-# pbs_allocate="-l nodes=1:ppn=9:gpus=1,partition=gpu,walltime=24:00:00 "
-# pbs_allocate="-l nodes=1:ppn=9:gpus=1,partition=gpu,walltime=01:00:00 "
-# pbs_allocate="-l nodes=1:ppn=4,walltime=01:00:00 "
 
 # Config Parameters ======================================================
 TRAINING_SCRIPT="AS_train.sh" 
 config="../yamls/chembl_cb29_train_1task.yaml"
 datadir="../../MLDatasets/chembl29"
-# outdir="../../experiments/AdaSparseChem-cb29"
 
 # Training Parms =========================================================
-# lr_list=(0.001)
+
 lr_list=(0.001)
 batch_size=4096
 
@@ -33,14 +24,13 @@ num_layers_list=(2)
 # num_layers_list=(1 2 3)
 dropout_list=(0.80)
  
-epochs=150
+epochs=10
 seed_idx=1
 ## Kusanagi: 0 --> 1, 1 --> 2; 2 --> 0
 cuda_device_id=1
 py_threads=6
 dev=0
-# dropout_list=(0.40 0.50 0.60)
-# dropout_list=( 0.70  0.80  0.90)
+
 #=========================================================================
 
 . submit_training_job.sh  $arch_type  $submit_type
